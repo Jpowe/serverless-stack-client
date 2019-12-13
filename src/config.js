cdbc5ee55214b7178ae5fa63916a624ae483e9aa
Jpowe <dearjoe@gmail.com>
2019-12-13 10:31:49 -0700

    setting up

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-v1"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "Yhttps://h9b3c7i4kc.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_qeZdOY71w",
+    APP_CLIENT_ID: "4o5kg7vpmvpu5imhui9o45ekur",
+    IDENTITY_POOL_ID: "us-east-1:f04660c7-470f-4ee6-99e5-77de1dcb4f4e"
+  }
+};
